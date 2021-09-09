3e95539cf9122f0a48438da28755b6740a6a7f8a
Kevin Wang <kwangsan@gmail.com>
2021-09-09 12:20:52 -0400

    feat: basic functionality

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,77 @@
-// https://vercel.com/docs/runtimes#official-runtimes/node-js/using-type-script-with-the-node-js-runtime
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-import { VercelRequest, VercelResponse } from "@vercel/node";
+import { createNodeMiddleware, createProbot } from "probot";
+import { ApplicationFunction } from "probot/lib/types";
 
-export default function (req: VercelRequest, res: VercelResponse) {
-  const { name = "World" } = req.query;
-  res.send(`Hello ${name}!`);
+/**
+ * @todo extract
+ */
+const probot = createProbot({
+  defaults: {
+    appId: process.env.APP_ID,
+    privateKey: process.env.PRIVATE_KEY,
+    secret: process.env.WEBHOOK_SECRET,
+  },
+});
+
+/**
+ * @todo extract
+ */
+const appFn: ApplicationFunction = (app) => {
+  app.on(["push"], async (ctx) => {
+    console.log(ctx.payload.repository.name);
+    return "push_TEST";
+  });
+  const addComment = `
+mutation comment($id: ID!, $body: String!) {
+  addComment(input: {subjectId: $id, body: $body}) {
+    clientMutationId
+  }
 }
+`;
+  // from the docs
+  app.on("issues.opened", async (ctx) => {
+    // Post a comment on the issue
+    await ctx.octokit.graphql(addComment, {
+      id: ctx.payload.issue.node_id,
+      body: "Hello World",
+    });
+  });
+  app.on("pull_request.opened", async (ctx) => {
+    ctx.payload.repository.name;
+    await ctx.octokit.request(
+      //https://docs.github.com/en/rest/reference/pulls#create-a-review-for-a-pull-request
+      "POST /repos/{owner}/{repo}/pulls/{pull_number}/reviews",
+      {
+        owner: ctx.payload.repository.owner.login,
+        repo: ctx.payload.repository.name,
+        pull_number: ctx.payload.pull_request.number,
+        body: JSON.stringify(ctx),
+        event: "COMMENT",
+      }
+    );
+  });
+  app.on("pull_request.synchronize", async (ctx) => {
+    await ctx.octokit.request(
+      "POST /repos/{owner}/{repo}/pulls/{pull_number}/reviews",
+      {
+        owner: ctx.payload.repository.owner.login,
+        repo: ctx.payload.repository.name,
+        pull_number: ctx.payload.pull_request.number,
+        body: JSON.stringify(ctx),
+        event: "COMMENT",
+      }
+    );
+  });
+};
+
+const eventHandler = createNodeMiddleware(appFn, {
+  probot,
+  webhooksPath: "/api/hello",
+});
+
+const handler = async (req: VercelRequest, res: VercelResponse) => {
+  eventHandler(req, res);
+};
+
+export default handler;
