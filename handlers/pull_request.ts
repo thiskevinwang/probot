9a73de9c99068dabf8110fc0227ad87ea8496c3c
Kevin Wang <kwangsan@gmail.com>
2021-10-05 02:08:40 -0400

    refactor: pulls -> issues

--- handlers/pull_request.ts
+++ handlers/pull_request.ts
@@ -10,14 +10,23 @@ export default <Fn>async function (ctx) {
   // payload.action: opened | synchronize
 
   await ctx.octokit.request(
-    //https://docs.github.com/en/rest/reference/pulls#create-a-review-for-a-pull-request
-    "POST /repos/{owner}/{repo}/pulls/{pull_number}/reviews",
+    "POST /repos/{owner}/{repo}/issues/{issue_number}/comments",
     {
       owner: ctx.payload.repository.owner.login,
       repo: ctx.payload.repository.name,
-      pull_number: ctx.payload.pull_request.number,
+      issue_number: ctx.payload.pull_request.number,
       body: body,
-      event: "COMMENT",
     }
   );
+  // await ctx.octokit.request(
+  //   //https://docs.github.com/en/rest/reference/pulls#create-a-review-for-a-pull-request
+  //   "POST /repos/{owner}/{repo}/pulls/{pull_number}/reviews",
+  //   {
+  //     owner: ctx.payload.repository.owner.login,
+  //     repo: ctx.payload.repository.name,
+  //     pull_number: ctx.payload.pull_request.number,
+  //     body: body,
+  //     event: "COMMENT",
+  //   }
+  // );
 };
